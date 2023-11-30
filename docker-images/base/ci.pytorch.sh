b267647a2bacc18e977c607a77090adc57b458e9
ariqbasyar <ariqbasyar2@gmail.com>
2023-11-30 08:22:35 +0700

    base docker images for opencv, pytorch, and yolo

--- docker-images/base/ci.pytorch.sh
+++ docker-images/base/ci.pytorch.sh
@@ -1,5 +1,8 @@
+set -x
+echo $DREG
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
-    -t ariqbasyar/pytorch:py3.9.18 \
+    --build-arg DREG=$DREG \
+    -t ${DREG}ariqbasyar/pytorch:v2.1.1-py3.9.18 \
     -f docker-images/base/Dockerfile.pytorch \
     --push .
--- docker-images/base/ci.yolo.sh
+++ docker-images/base/ci.yolo.sh
@@ -3,6 +3,6 @@ echo $DREG
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
     --build-arg DREG=$DREG \
-    -t ${DREG}ariqbasyar/opencv:v4.8.1.78-py3.9.18 \
-    -f docker-images/base/Dockerfile.opencv \
+    -t ${DREG}ariqbasyar/pytorch:v2.1.1-py3.9.18-yolov7 \
+    -f docker-images/base/Dockerfile.yolo \
     --push .
