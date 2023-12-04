22d5cff8b1cf5a7c18cb79049c5100d9341e0cdf
ariqbasyar <ariqbasyar2@gmail.com>
2023-12-04 14:45:32 +0700

    edit command to run user to support both FogBus2 and FogVerse packaged in a single image

--- input/ci.sh
+++ input/ci.sh
@@ -3,7 +3,7 @@ echo $DREG
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
     --build-arg DREG=$DREG \
-    -t ${DREG}ariqbasyar/fogbus2-fogverse:user \
+    -t ${DREG}ariqbasyar/fogbus2-fogverse:user-0 \
     -f Dockerfile \
     --push ..
-docker pull ${DREG}ariqbasyar/fogbus2-fogverse:user
+docker pull ${DREG}ariqbasyar/fogbus2-fogverse:user-0
