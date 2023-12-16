c61cf505659ffa3c02fe4f317fffc9f4fa5ed7dd
ariqbasyar <ariqbasyar2@gmail.com>
2023-12-16 21:29:22 +0700

    now executor need no storage after always seek to the end update

--- executor/inference.py
+++ executor/inference.py
@@ -5,7 +5,7 @@ import torch
 import numpy as np
 
 from pathlib import Path
-from fogverse import Consumer, Producer, ConsumerStorage, CsvLogging
+from fogverse import Consumer, Producer, CsvLogging
 
 ENCODING = os.getenv('ENCODING', 'jpg')
 MODEL = os.getenv('MODEL', 'yolo7crowdhuman.pt')
@@ -13,26 +13,20 @@ SCHEME = os.getenv('SCHEME', '1250-1255')
 
 CSV_DIR = Path('logs') / SCHEME
 
-class MyExecutorStorage(CsvLogging, Consumer, ConsumerStorage):
-    def __init__(self, keep_messages=False):
-        self.consumer_topic = [f'input_{SCHEME}']
-        csv_file = f'{self.__class__.__name__}_{SCHEME}.csv'
-        CsvLogging.__init__(self, filename=csv_file, dirname=CSV_DIR)
-        Consumer.__init__(self)
-        ConsumerStorage.__init__(self, keep_messages=keep_messages)
-
-class MyExecutor(CsvLogging, Producer):
-    def __init__(self, consumer):
+class MyExecutor(CsvLogging, Consumer, Producer):
+    def __init__(self):
         # Using https://github.com/WongKinYiu/yolov7
         # from commit 84932d70fb9e2932d0a70e4a1f02a1d6dd1dd6ca
         self.model = torch.hub.load('yolov7', 'custom',
                                     MODEL, source='local')
+        self.consumer_topic = [f'input_{SCHEME}']
         self.producer_topic = f'result_{SCHEME}'
-        self.consumer = consumer
         self.encode_encoding = 'jpg'
+        self.always_read_last = True
 
         csv_file = f'{self.__class__.__name__}_{SCHEME}.csv'
         CsvLogging.__init__(self, filename=csv_file, dirname=CSV_DIR)
+        Consumer.__init__(self)
         Producer.__init__(self)
 
     async def _after_start(self):
@@ -41,9 +35,6 @@ class MyExecutor(CsvLogging, Producer):
         print('warming up')
         self.model(dummy)
         print('ready')
-
-    async def receive(self):
-        return await self.consumer.get()
 
     def _process(self, data):
         results = self.model(data)
@@ -56,9 +47,8 @@ class MyExecutor(CsvLogging, Producer):
                                                data)
 
 async def main():
-    consumer = MyExecutorStorage()
-    producer = MyExecutor(consumer)
-    tasks = [consumer.run(), producer.run()]
+    producer = MyExecutor()
+    tasks = [producer.run()]
     try:
         await asyncio.gather(*tasks)
     finally:
